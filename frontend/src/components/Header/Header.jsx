26ed3a6719b3944f0468094927398d0af99dc6c7
Daniel Cursi <danielfreitasxdy@gmail.com>
2026-08-03 20:30:49 -0300

    concluido icones do menu

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,31 +1,40 @@
 import { Link } from "react-router-dom"
 import styles from "./styles.module.css"
+import { IoIosHome, IoIosPerson } from "react-icons/io";
+import { FaBoxArchive, FaArrowRightArrowLeft, FaWarehouse, FaHelmetSafety } from "react-icons/fa6";
+import { FaBoxOpen, FaUser } from "react-icons/fa";
+import { BiExclude } from "react-icons/bi";
+import { FcElectroDevices } from "react-icons/fc";
+
 
 export default function Header(){
     return(
         <header className={styles.header}>
             <nav className={styles.nav}>
                 <section className={styles.logo}>
-                    <h4>Controle Comercial</h4>
-                    <p>Gestão de material rastreavel</p>
+                    <FcElectroDevices className={styles.icon}/>
+                    <div>
+                       <h4>Controle Comercial</h4>
+                        <p>Gestão de material rastreavel</p> 
+                    </div>
                 </section>
                 <section className={styles.links}>
                     <h4>Principal</h4>
-                    <Link to={"/"}>Inicio</Link>
-                    <Link to={"/equipment"}>Estoque</Link>
+                    <Link to={"/"}><IoIosHome />Inicio</Link>
+                    <Link to={"/equipment"}><FaBoxArchive />Estoque</Link>
                 </section>
                 <section className={styles.links}>
                     <h4>Operações</h4>
-                    <Link to={"/transfer"}>Transferências</Link>
+                    <Link to={"/transfer"}><FaArrowRightArrowLeft />Transferências</Link>
                 </section>
                 <section className={styles.links}>
                     <h4>Cadastros</h4>
-                    <Link to={"/center"}>Centros Logísticos</Link>
-                    <Link to={"/team"}>Turmas</Link>
-                    <Link to={"/electrician"}>Eletricistas</Link>
-                    <Link to={"/material"}>Materiais</Link>
-                    <Link to={"/brand"}>Marcas</Link>
-                    <Link to={"/user"}>Usuarios</Link>
+                    <Link to={"/center"}><FaWarehouse />Centros Logísticos</Link>
+                    <Link to={"/team"}><FaHelmetSafety />Turmas</Link>
+                    <Link to={"/electrician"}><IoIosPerson />Eletricistas</Link>
+                    <Link to={"/material"}><FaBoxOpen />Materiais</Link>
+                    <Link to={"/brand"}><BiExclude />Marcas</Link>
+                    <Link to={"/user"}><FaUser />Usuarios</Link>
                 </section>
             </nav>
         </header>
