80f1b8c9542418284903b3ec5f472097439dc835
Daniel Cursi <danielfreitasxdy@gmail.com>
2026-08-03 21:53:00 -0300

    integracao front e back iniciada

--- frontend/src/pages/Equipment/Equipment.jsx
+++ frontend/src/pages/Equipment/Equipment.jsx
@@ -1,4 +1,22 @@
+import { useEffect, useState } from "react"
+import api from "../../services/api"
+
 export default function Equipment(){
+    const [equipamento, setEquipamento] = useState([])
+
+    useEffect(() => {
+        buscarEquipamentos()
+    }, [])
+
+    const buscarEquipamentos = async () => {
+        try {
+            const response = await api.get('/equipment')
+            console.log(response.data)
+            setEquipamento(response.data)
+        } catch (error){
+            console.log("Erro ao buscar o estoque: ", error)
+        }
+    }
     return(
         <h1>Estoque</h1>
     )
