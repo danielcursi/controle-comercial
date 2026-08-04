e3652557e253f1250a3d2e806bb3d73f868cf625
Daniel Cursi <danielfreitasxdy@gmail.com>
2026-08-03 21:56:43 -0300

    adicionar cors

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from 'cors'
 
 import teamRouter from './routes/team.routes.js'
 import centerRouter from './routes/centers.routes.js'
@@ -11,6 +12,7 @@ import transferRouter from './routes/transfer.routes.js'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/team', teamRouter)
